dba22caf5e8677341b82414b2feb5372fc2d54be
ethan.edwards <edwardet@onid.oregonstate.edu>
2015-04-15 20:52:29 -0700

    Fixed CompPCA code

--- CompPCA.R
+++ CompPCA.R
@@ -8,9 +8,10 @@ stars <- read.arff(file = 'AstronomyTrainingData.txt')
 
 starcom <- stars %>% filter(complete.cases(.))
 starcom.std <- starcom
-for (i in (2:86)) {
+for (i in c(2:32,34:41,43:50,52:86)) {
   starcom.std[,i] <- (starcom[,i] - mean(starcom[,i])) / sd(starcom[,i])
 }
+starcom.std2 <- starcom.std %>% filter(complete.cases(.))
 
 # a weird note: some star classes are dramatically reduced
 # by removing all NAs. in particular, class 25 goes from
@@ -26,5 +27,12 @@ j=1; for (i in unique(stars$class)) {
 # this creates 25 "pca.#" lists. we can analyze any one individually.
 # replace each "(I)" with the desired class number:
 
+summary(pca.9)
 pca.9.latent.sem <- pca.9$rotation
-sort(pca.9.latent.sem[,1],decreasing=FALSE)[1:30]
+sort(pca.9.latent.sem[,1],decreasing=TRUE)[1:30]
+
+signif(sort(pca.9.latent.sem[,1],decreasing=TRUE)[1:30],2)
+signif(sort(pca.9.latent.sem[,1],decreasing=FALSE)[1:30],2)
+
+signif(sort(pca.9.latent.sem[,2],decreasing=TRUE)[1:30],2)
+signif(sort(pca.9.latent.sem[,2],decreasing=FALSE)[1:30],2)
