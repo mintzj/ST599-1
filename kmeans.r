b469dc48882dd082fc0102f3245eebaecb14f5df
unknown <mintzj@KIDD086-PC01.science.oregonstate.edu>
2015-04-18 21:23:36 -0700

    Kmeans updated to use our specified clusters

--- kmeans.r
+++ kmeans.r
@@ -17,55 +17,139 @@ star_names <- levels(stars$class)
 levels(stars$class)
 barplot(summary(stars$class), horiz = TRUE, las = 1, cex.names = 0.5)
 
-?kmeans
-#numCenters<-5
-#kmeansCancer<-kmeans(t(cancerData), numCenters, iter.max = 20, nstart = 1)
-
-#  I made 25 centers, there's no reason there need to be 25, it could be any number.
+#  We could specify that there are 25 centers, and kmeans will attempt to 
+#  locate 25 good centers, but it may be terrible!
 centers <- 25
 
-
 star_km <- kmeans(complete_stars[c(-1,-dim(complete_stars)[2])], centers, iter.max = 20, nstart = 1)
-summary(star_km)
-str(star_km)
 found_centers <- star_km$centers
+
 #  There are 25 centers, one for each cluster.  The center contains 85 means representing each variable.
+#  Cluster membership is identified by a a number.
+# View(star_km$cluster)
+
+#Numbers of stars per cluster
+table(factor(star_km$cluster))
+plot(table(factor(star_km$cluster)),xlab = "Cluster Number", ylab = "Stars per Cluster")
+
+
+
 
 # I can make one of these by:  
 #  1)  Sorting the data by star type
 #  2)  taking the mean for each star type, for each variable.
-#  mapply?
-grouped_complete_stars <- group_by(complete_stars, class)
-sum_stars <- summarise(grouped_complete_stars, amplitude = mean(amplitude))
-#sum_stars <- complete_stars %>% group_by(class) %>% summarise_each(funs(mean))
-sum_stars <- stars %>% group_by(class) %>% summarise_each(funs(mean(., na.rm=TRUE)))
 
-summarise(grouped_complete_stars, amplitude = mean(amplitude))
-df %>% group_by(grp) %>% summarise_each(funs(mean))
-#df %>% group_by(grp) %>% summarise_each(funs(mean))
+# with dplyr:  group COMPLETE stars by class, then summarise with the mean across all variables.
+#sum_stars <- complete_stars %>% group_by(class) %>% summarise_each(funs(mean))
+
+# with dplyr:  group each star by class, then summarise all the variables with mean(x, rm.na)
+sum_stars <- stars %>% group_by(class) %>% summarise_each(funs(mean(., na.rm=TRUE)))
 
 #star_km$size
 #star_km$centers
 #star_km$tot.withinss
 #table(factor(star_km$cluster), factor(unlist(star_names)))
 
+#full_star_km  <-  kmeans(complete_stars[c(-1,-dim(complete_stars)[2])], centers = sum_stars, 
+ #                        iter.max = 20, nstart = 1)
 
-#length(star_names)
-star_km$cluster
-table(factor(star_km$cluster))
-hist(star_km$cluster, 25)
+# Setting our own centers ---------------------------
+# We had to switch to "Lloyd" method, as "Hartigan-Wong" cannot handle size 0 clusters.
+full_star_km  <-  kmeans(as.matrix(complete_stars[,c(-1,-87)]), 
+                         centers = as.matrix(sum_stars[,c(-1,-2)]), iter.max = 20,
+                         algorithm = "Lloyd")
 
-#6 clusters:
-centers <- 10
-star_km <- kmeans(complete_stars[c(-1,-dim(complete_stars)[2])], centers, iter.max = 20, nstart = 1)
-#star_km$cluster
-#table(factor(star_km$cluster))
-hist(star_km$cluster, centers)
+# Result of clustering with 25 random clusters
+plot(table(star_km$cluster))
+table(star_km$cluster)
+# Result of clustering with 25 nonrandom clusters drawn from training.
+table(full_star_km$cluster) 
+plot(table(full_star_km$cluster))
+plot(table(stars$class))
+barplot(summary(stars$class), horiz = TRUE, las = 1, cex.names = 0.5)
+
+
+
+#algorithm = c("Hartigan-Wong", "Lloyd", "Forgy","MacQueen"), trace=FALSE)
+View(as.matrix(sum_stars[1:10,c(-1,-2)]))
+
+
+is.data.frame(complete_stars)
+View(sum_stars)
+?kmeans
+
+View(sum_stars[,c(-1,-2,-87)])
+dim(sum_stars[,c(-1,-2)])
+dim(stars[,c(-1,-87)])
+
+
+# An example of setting the centers ---------------------------
+#  Available here:  http://stats.stackexchange.com/questions/72606/k-means-clustering-after-first-iteration
+
+# Set the RNG seed to ensure reproducibility
+set.seed(12345)
+
+# Let's create 3 visually distinct clusters
+n <- c(1000, 500, 850)
+# Jeff: X var defined here for 3 clusters
+classifier.1 <- c(rnorm(n[1], 10, 0.9), 
+                  rnorm(n[2], 25, 2),
+                  rnorm(n[3], 35, 2))
+# Jeff:  Y var defined here for 3 clusters.
+classifier.2 <- c(rnorm(n[1], 5, 1),
+                  rnorm(n[2], 10, 0.4),
+                  rnorm(n[3], 2, .9))
+
+col = c("blue", "darkgreen", "darkred")
+# Run k-means with 3 clusters and random initial centroids 
+# to check the clusters are correctly recognized
+km <- kmeans(cbind(classifier.1, classifier.2), 3)
+# Plot the data, colored by cluster
+plot(classifier.1, classifier.2, pch=20, col=col[km$cluster])
+
+# Mark the final centroids
+points(km$centers, pch=20, cex=2, col="orange")
+
+# Now impose some obviously "wrong" starting centroids
+start.x <- c(10, 25, 3000)
+start.y <- c(10, 10, -10000)
+km.2 <- kmeans(cbind(classifier.1, classifier.2), 
+               centers=cbind(start.x, start.y))
+
+# Jeff:  How will this preform if we give it the right stuff?
+start.x <- c(10, 25, 35)
+# It still finds it even if we give pretty bad y values!
+start.y <- c(0, 1, 3)
+km.2 <- kmeans(cbind(classifier.1, classifier.2), 
+               centers=cbind(start.x, start.y))
+plot(classifier.1, classifier.2, pch=20, col=col[km.2$cluster])
+points(km.2$centers, pch=20, cex=2, col="orange")
+
+View(cbind(start.x, start.y))
+?kmeans
+
+# Working with the whole data ---------------------------
+# The location of this file will vary for you.  I didn't upload it to github, since it is so large.
+# I suppose we could upload it if we want.
+
+all_stars <- read.arff(file = 'Z:/ST599/Project 1/data/AstronomyTestData.txt')
+
+
+
+
+
+
+
+
+
+# Multicolinearity Analysis ---------------------------
 
 #Is there multicolinearity?
-install.packages('faraway')
+
+# install.packages('faraway')
 library(faraway)
 #Calculate the variance inflation factors (a measure of multicolinearity)
+#  (VIFS are a ratio of eigenvalues to the smallest eigenvalue)
 complete_stars_noname <- complete_stars[c(-1,-dim(complete_stars)[2])]
 vifs <- (vif(complete_stars_noname))
 vifs
@@ -102,6 +186,12 @@ low_vif <- (vif(star_low_vif))
 plot(low_vif)
 identify(low_vif)
 View(star_low_vif[,c(1,81,82)])
+
+
+
+
+
+
 # Sharm's Example ---------------------------
 
 #cancerData<-read.table(file="14cancer.xtrain",sep="")
